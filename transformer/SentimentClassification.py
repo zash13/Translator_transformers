6ff53aac055ff42e56c0f8058c8751bcd3c68766
k13 <mihome013@gmail.com>
2025-08-18 19:00:53 +0330

    feat : connect preprocessing and transformer togather , using imdb dataset

--- transformer/SentimentClassification.py
+++ transformer/SentimentClassification.py
@@ -1,52 +1,41 @@
-import tensorflow as tf
+from embeding_positionaleEncoding import preprocessing
+from transformerBlock import TransformerBlock
 import keras
+from keras.preprocessing import sequence
 from keras import layers
-import numpy as np
-
-# this is my first project on transformers , basicly its using others code to learn how thing work in here
-# this is where you can find where the origin of the code come from :
-# https://keras.io/examples/nlp/text_classification_with_transformer/
 
 
-class TransformerBlock(layers.Layer):
-    def __init__(self, embeding_dim, num_heads, feadForward_dim, rate=0.1):
-        super().__init__()
-        # its compute how much each token should pay attentoin to others
-        self.attention_layer = layers.MultiHeadAttention(
-            num_heads=num_heads, key_dim=embeding_dim
-        )
+VOCAB_SIZE = 20000
+MAX_LEN = 200
+EMBEDDING_DIM = 64
+NUM_HEADS = 2
+FF_DIM = 64
 
-        # this layer is on each toekn , while attention mixes the information across tokens this layers help each token to have its own idea
-        # like attention care about sequence , while each part of sequence need to attent to for it self
-        # if i want to put it in an example , its like every toeken know the idea of the sequence , but each one hase its own thouts too!!!
-        # like people in same party  , which all know the general idea of the party , but each one hase its own thinking too( if so :) )
-        self.feedforward_network = keras.Sequential(
-            [
-                layers.Dense(feadForward_dim, activation="relu"),
-                layers.Dense(embeding_dim),
-            ]
-        )
-        # still unfamilier with normalizatoin , but i know it for facat that Dropout is going to to Dropout some neurons :))))) like turning them off !
-        # and preventing from overfitting
-        self.layernorm1 = layers.LayerNormalization(epsilon=1e-6)
-        self.layernorm2 = layers.LayerNormalization(epsilon=1e-6)
-        self.dropout1 = layers.Dropout(rate)
-        self.dropout2 = layers.Dropout(rate)
+(x_train, y_train), (x_val, y_val) = keras.datasets.imdb.load_data(num_words=VOCAB_SIZE)
+print(len(x_train), "Training sequences")
+print(len(x_val), "Validation sequences")
 
-    def feedforward(self, inputs):
-        # if you not get this part to , read articles about differece between self attention and attention( i think so )
-        # i think in self attention , each token look at all tokens including itself , so query = key = value = input
-        # while in attention it self , we have Q and V and K
-        # like in cross-attention (better name then attention is self) we have Q that comes from oen source while K and V comes from another
-        # as an example , gpt told me in encode-decoder attention which i am not implement it nor see it yet ( i even dont know whats it do )
-        # Q comes from decoder while K and V comes from encoder
-        # key=none -> key=value
-        attention_output = self.attention_layer(query=inputs, value=inputs)
-        attention_output = self.dropout1(attention_output)
-        # now each toekn aware of whole sentence how ? i dont know :)
-        #
-        # look at the figur 1 in attentoins is all you need paper , you will find this layer there
-        out1 = self.layernorm1(inputs + attention_output)
-        fnn_output = self.feedforward_network(out1)
-        fnn_output = self.dropout2(fnn_output)
-        return self.layernorm2(out1 + fnn_output)
+x_train = sequence.pad_sequences(x_train, maxlen=MAX_LEN)
+x_val = sequence.pad_sequences(x_val, maxlen=MAX_LEN)
+
+print("x_train shape:", x_train.shape)
+print("x_val shape:", x_val.shape)
+inputs = keras.Input(shape=(MAX_LEN,))
+
+x = preprocessing(vocab_size=VOCAB_SIZE, embedding_dim=EMBEDDING_DIM, max_len=MAX_LEN)(
+    inputs
+)
+x = TransformerBlock(EMBEDDING_DIM, NUM_HEADS, FF_DIM)(x)
+x = layers.GlobalAveragePooling1D()(x)
+x = layers.Dropout(0.1)(x)
+x = layers.Dense(20, activation="relu")(x)
+x = layers.Dropout(0.1)(x)
+outputs = layers.Dense(1, activation="sigmoid")(x)
+
+model = keras.Model(inputs=inputs, outputs=outputs)
+
+model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+model.summary()
+history = model.fit(
+    x_train, y_train, batch_size=64, epochs=2, validation_data=(x_val, y_val)
+)
