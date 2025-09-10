b7f96179bd475f17511227718b41563f087028a3
k13 <mihome013@gmail.com>
2025-09-10 23:50:34 +0330

    fix : miss speld name

--- transformer/Translator/EncoderBlock.py
+++ transformer/Translator/EncoderBlock.py
@@ -5,11 +5,11 @@ from keras import layers
 # The EncoderBlock has the same idea as the block I created in TransformerBlock.
 # To clarify, I renamed it so that it matches the terminology used in the articles.
 class EncoderBlock(layers.Layer):
-    def __init__(self, embeding_dim, num_heads, feadForward_dim, rate=0.1):
+    def __init__(self, embedding_dim, num_heads, feadForward_dim, rate=0.1):
         super().__init__()
 
         self.encoderBlock = TransformerBlock(
-            embeding_dim, num_heads, feadForward_dim, rate=rate
+            embedding_dim, num_heads, feadForward_dim, rate=rate
         )
 
     def call(self, input):
