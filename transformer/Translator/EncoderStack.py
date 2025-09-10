7b94b68814b283039a2e5fe27d857ee5519afe1d
k13 <mihome013@gmail.com>
2025-09-10 23:53:00 +0330

    feat : wrote encoderStack

---
+++ transformer/Translator/EncoderStack.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+from keras import layers
+from EncoderBlock import EncoderBlock
+
+
+class EncoderStack(layers.Layer):
+    def __init__(
+        self, num_layers, embedding_dim, num_heads, feed_forward_dim, rate=0.1
+    ):
+        super().__init__()
+        self.num_layers = num_layers
+        self.encoder_stack = [
+            EncoderBlock(embedding_dim, num_heads, feed_forward_dim, rate)
+            for _ in range(num_layers)
+        ]
+
+    def call(self, inputs):
+        x = input
+        for layer in self.encoder_stack:
+            x = layer(x)
+        return x
