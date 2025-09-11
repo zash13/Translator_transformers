fix : change masking method in translator model and change the main move it to test

transformer/Translator/TranslatorModel.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
 import keras as ks
-from PositionalEncoding import positional_encoding
 from TokenEmbedding import TokenEmbedding
 from EncoderStack import EncoderStack
 from DecoderStack import DecoderStack
+from PositionalEncoding import positional_encoding
 
 
 class Translator(ks.Model):
@@ -17,7 +17,7 @@ class Translator(ks.Model):
         target_vocab_size,
         max_sequence_length,
         rate=0.1,
-    ) -> None:
+    ):
         super().__init__()
         self.encoder_embedding = TokenEmbedding(input_vocab_size, embedding_dim)
         self.decoder_embedding = TokenEmbedding(target_vocab_size, embedding_dim)
@@ -30,32 +30,27 @@ class Translator(ks.Model):
         self.decoder = DecoderStack(
             num_layers, embedding_dim, num_heads, feed_forward_dim, rate
         )
-        self.final_layer = ks.layers.Dense(target_vocab_size)
+        self.final_layer = tf.keras.layers.Dense(target_vocab_size)
+
+    def build(self, input_shape):
+        # input_shape: [(batch, enc_seq_len), (batch, dec_seq_len)]
+        enc_input_shape, dec_input_shape = input_shape
+        self.encoder_embedding.build(enc_input_shape)
+        self.decoder_embedding.build(dec_input_shape)
+        self.encoder.build((None, None, self.encoder_embedding.embedding_dim))
+        self.decoder.build((None, None, self.decoder_embedding.embedding_dim))
+        self.final_layer.build((None, None, self.decoder_embedding.embedding_dim))
+        self.built = True
 
     def call(self, inputs, training=True):
-        enc_inputs, dec_inputs = inputs  # Expect [encoder_input, decoder_input]
-        """
-        enc_padding_mask = self.create_padding_mask(enc_inputs)
-        dec_padding_mask = self.create_padding_mask(dec_inputs)
-        look_ahead_mask = self.create_look_ahead_mask(tf.shape(dec_inputs)[1])
-        dec_self_mask = tf.maximum(dec_padding_mask, look_ahead_mask)
-        """
+        enc_inputs, dec_inputs = inputs
         enc_emb = self.encoder_embedding(enc_inputs)
         enc_emb += self.positional_encoding[:, : tf.shape(enc_emb)[1], :]
         enc_output = self.encoder(enc_emb)
 
         dec_emb = self.decoder_embedding(dec_inputs)
         dec_emb += self.positional_encoding[:, : tf.shape(dec_emb)[1], :]
-
         dec_output = self.decoder(dec_emb, enc_output)
 
         final_output = self.final_layer(dec_output)
         return final_output
-
-    def create_padding_mask(self, seq):
-        mask = tf.cast(tf.math.equal(seq, 0), tf.float32)
-        return mask[:, tf.newaxis, tf.newaxis, :]
-
-    def create_look_ahead_mask(self, size):
-        mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
-        return mask

transformer/Translator/main.py
@@ -1,41 +1,6 @@
 import numpy as np
-import pandas as pd
 import tensorflow as tf
 from TranslatorModel import Translator
-
-
-def test():
-    num_layers = 2
-    embedding_dim = 128
-    num_heads = 4
-    feed_forward_dim = 512
-    input_vocab_size = 1000
-    target_vocab_size = 1000
-    max_sequence_length = 20
-    batch_size = 64
-    seq_len = 10
-
-    enc_inputs = tf.random.uniform(
-        (batch_size, seq_len), maxval=input_vocab_size, dtype=tf.int32
-    )
-    dec_inputs = tf.random.uniform(
-        (batch_size, seq_len), maxval=target_vocab_size, dtype=tf.int32
-    )
-
-    model = Translator(
-        num_layers,
-        embedding_dim,
-        num_heads,
-        feed_forward_dim,
-        input_vocab_size,
-        target_vocab_size,
-        max_sequence_length,
-    )
-
-    output = model([enc_inputs, dec_inputs])
-    print("Input shapes:", enc_inputs.shape, dec_inputs.shape)
-    print("Output shape:", output.shape)
-
-
-if __name__ == "__main__":
-    test()
+from TokenEmbedding import TokenEmbedding
+from EncoderStack import EncoderStack
+from DecoderStack import DecoderStack