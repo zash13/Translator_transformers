c17aa8dfaa3bdbca24063ffdee61cd39bdb6390f
k13 <mihome013@gmail.com>
2025-10-10 08:36:28 +0330

    update : add build pattern to tokenizers

--- transformer/Translator/test_tokenizer2.py
+++ transformer/Translator/test_tokenizer2.py
@@ -1,14 +1,15 @@
+import os
+from abc import ABC, abstractmethod
+from enum import Enum, auto
+from typing import List
+
+import pandas as pd
 from hazm import Normalizer, word_tokenize
 from tokenizers import Tokenizer
 from tokenizers.models import WordPiece
-from tokenizers.trainers import WordPieceTrainer
 from tokenizers.pre_tokenizers import Whitespace
-import pandas as pd
-import os
-
-from abc import ABC, abstractmethod
-from typing import List
-from enum import Enum, auto
+from tokenizers.trainers import WordPieceTrainer
+from typing import Dict
 
 
 class TokenizerType(Enum):
@@ -62,6 +63,15 @@ class SpecialToken(Enum):
 
 
 class BaseTokenizer(ABC):
+    @classmethod
+    def required_args(cls) -> Dict[str, str]:
+        """return a list of required arguments for building this tokenizer"""
+        pass
+
+    @abstractmethod
+    def build(self, **kwargs):
+        raise NotImplementedError
+
     @abstractmethod
     def tokenize(self, text: str) -> List[str]:
         """Tokenize text into tokens"""
@@ -99,7 +109,35 @@ class PersianHazmTokenizer(BaseTokenizer):
             self.vocab.setdefault(tok.token_str, tok.value)
             self.inv_vocab[tok.value] = tok.token_str
 
-    def build_vocab(self, texts, min_freq=2):
+    @classmethod
+    def required_args(cls):
+        return {
+            "texts": "a list of the sentence  , used to excute the word and create the vocab",
+            "min_freq": "how aften tokenizer need to see a word to add it to vocab if it set to 2 : then each word need to seen at list 2 time",
+        }
+
+    # this may not needed , like it can simply writen like this ; return self.train_by_text(**kwargs)
+    def build(self, **kwargs):
+        texts = kwargs.get("texts")
+        min_freq = kwargs.get("min_freq", 2)
+        if texts:
+            self.train_by_text(texts, min_freq)
+        return self
+
+    def train_by_text(self, texts, min_freq) -> bool:
+        """
+        use this method to create your tokens :
+        texts: a list of the sentence  , used to excute the word and create the vocab
+        min_freq : how aften tokenizer need to see a word to add it to vocab if it set to 2 : then each word need to seen at list 2 time
+        """
+        try:
+            self.tokenizer = self._build_vocab(texts, min_freq)
+            return True
+        except Exception as e:
+            print(f"Something wrong happened: {e}")
+            return False
+
+    def _build_vocab(self, texts, min_freq):
         normalizer = Normalizer()
         freq = {}
 
@@ -108,10 +146,10 @@ class PersianHazmTokenizer(BaseTokenizer):
             for t in tokens:
                 freq[t] = freq.get(t, 0) + 1
 
-        self.vocab = {tok.token_str: tok.value for tok in SpecialToken}
-        idx = len(SpecialToken)
+        self.vocab = {tok.token_str: tok.value for tok in SpecialToken.all_tokens()}
+        idx = len(SpecialToken.all_tokens())
 
-        for t, c in sorted(freq.items(), key=lambda x: -x[1]):
+        for t, c in sorted(freq.items(), key=lambda x: x[1], reverse=True):
             if c >= min_freq:
                 self.vocab[t] = idx
                 idx += 1
@@ -156,9 +194,31 @@ class EnglishTokenizer(BaseTokenizer):
     def __init__(self, tokenizer=None):
         self.tokenizer = tokenizer
 
+    @classmethod
+    def required_args(cls):
+        return {
+            "texts": "a list of the sentence  , used to excute the word and create the vocab",
+            "vocab_size": "size of your vocab !",
+            "unk_token": "token that tokenizer will use for unknown word , default is SpecialToken.UNK",
+        }
+
+    def build(self, **kwargs):
+        texts = kwargs.get("texts")
+        vocab_size = kwargs.get("vocab_size")
+        unk_token = kwargs.get("unk_token", SpecialToken.UNK.token_str)
+        if texts and vocab_size:
+            self.train_by_text(texts, vocab_size, unk_token)
+        return self
+
     def train_by_text(
         self, texts, vocab_size, unk_token=SpecialToken.UNK.token_str
     ) -> bool:
+        """
+        use this method to create your tokens :
+        texts: a list of the sentence  , used to excute the word and create the vocab
+        vocab_size : size of your vocab !
+        unk_token : token that tokenizer will use for unknown word , default is SpecialToken.UNK
+        """
         try:
             self.tokenizer = self._create_english_tokenizer(
                 texts, vocab_size, unk_token
@@ -215,6 +275,40 @@ class EnglishTokenizer(BaseTokenizer):
         if self.tokenizer is None:
             raise ValueError("Tokenizer not trained. Call train_by_text first.")
         return self.tokenizer.get_vocab_size()
+
+
+class TokenizerBuilder:
+    """
+    A Builder class for creating and training tokenizers.
+
+    Example usage:
+
+        builder = TokenizerBuilder()
+        hazm_tokenizer = (
+            builder
+            .set_type(TokenizerType.HAZM)
+            .set_training_data(texts)
+            .set_min_freq(2)
+            .build()
+        )
+
+        english_tokenizer = (
+            builder
+            .set_type(TokenizerType.WORDPIECE)
+            .set_training_data(texts)
+            .set_vocab_size(5000)
+            .build()
+        )
+    """
+
+    def __init__(self) -> None:
+        pass
+
+    def CreateToeknizer(self, tokenizerType: TokenizerType) -> BaseTokenizer:
+        if tokenizerType == TokenizerType.HAZM:
+            return PersianHazmTokenizer
+        if tokenizerType == TokenizerType.WORDPIECE:
+            return EnglishTokenizer
 
 
 def create_english_tokenizer(texts, vocab_size):
